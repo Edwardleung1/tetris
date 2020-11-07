e7e239520dea36a25d888bf04428f1d943fab956
Edward Leung <edward_leung_12@hotmail.com>
2020-11-07 00:24:54 +0000

    parsed the divs into an array

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoader', () => {
-  
+  const grid = document.querySelector('.grid');
+  // Turn all the divs into an array
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+
+  console.log(squares);
 });
