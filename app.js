c9bbeeb07536ec169c7692c3a4acad40f3dbc644
Edward Leung <edward_leung_12@hotmail.com>
2020-11-07 00:50:25 +0000

    created the first lTetrominoe shape using an array with 4 arrays inside for each rotation

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoader', () => {
   const grid = document.querySelector('.grid');
   // Turn all the divs into an array
   let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
   const width = 10;
 
-  console.log(squares);
+  // The Tetrominoes
+  const lTetrominoes = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
 });
